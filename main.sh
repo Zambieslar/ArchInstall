1811879d9d88347133a303ea970bdb03c32b9203
Zambieslar <brandonj.pershall@gmail.com>
2024-06-22 23:23:38 -0600

    Added bootloader installation

--- main.sh
+++ main.sh
@@ -10,15 +10,16 @@ NETSTATUS=$(ping archlinlux.org)
 
 case "$?" in
         1)
-                printf "No connection currently available"
+                printf "No connection currently available. Please connect the device to the internet before continuing with the installation."
+                exit 1
                 ;;
         0)
-                printf "Connection established. Continuing with installation"
+                printf "Connection established. Continuing with installation."
                 ;;
 esac
 
 if [[ "$NTPSTATUS" = "inactive" ]]; then
-        printf "NTP currently inactive. This is not ideal in some situations as it will prevent package installations and system updtes. Fixing for your dumbass..."
+        printf "NTP currently inactive. This is not ideal in some situations as it will prevent package installations and system updates. Fixing for your dumbass..."
         sed -Ee 's/#NTP=//g' -e 's/(#FallbackNTP=)(.*)/NTP=\2/' /etc/systemd/timesyncd.conf
         timedatectl set-ntp true
 fi
@@ -31,7 +32,7 @@ read -r ENCRYPT
 case $DRIVE_TYPE in
         "LVM"|"Lvm"|"lvm"|1)
                 clear
-                printf "First, we need to partition at least two partitions on a drive.\n\nIf you choose to encrypt your LV's, the boot volume can't be encrypted or the bootloader won't be able to load our linux kernel on boot.\n\nCreate one boot partition and one partition to wrap your LV's"
+                printf "First, we need to create at least two partitions on a drive.\n\nIf you choose to encrypt your LV's, the boot volume can't be encrypted or the bootloader won't be able to load our the kernel on boot.\n\nCreate one boot partition and one root partition to wrap your Logical Volumes."
                 DRIVE=getDrives
                 partitionDrive $DRIVE # Passing block device in /dev/block_dev format
                 if [[ $ENCRYPT == "Yes" ]] || [[ $ENCRYPT == "yes" ]] || [[ $ENCRYPT == "1" ]]; then
@@ -51,28 +52,94 @@ do
         ((X++))
 done
 printf "\n(0-5) >>> "
-read KERNEL
+
 printf "Installing the base system"
 pacstrap -K /mnt "${PKGS[@]}" "$KERNEL"
-"Generating fstab to match partition layout"
+printf "Generating fstab to match partition layout"
 genfstab -U /mnt >> /mnt/etc/fstab
-clear
 printf "Please select a region\n\n"
 X=0
-for i in "${!ZONES[@]}"
+for i in "${REGIONS[@]}"
 do
-        MAP["$X"]="$i"
         printf "%s\n" "   $X) $i"
         ((X++))
 done
 printf "\n>>> "
 read -r REGION
+clear
 printf "Please select a zone\n\n"
-X=0
-for i in ${ZONES[${MAP[$REGION]}]}
+for i in "${ZONES[@]}"
 do
         printf "%s\n" "   $X) $i"
         ((X++))
 done
 printf "\n>>> "
 read -r ZONE
+
+printf "%s" "Setting timezone to ${REGIONS[$REGION]} - ${ZONES[$ZONE]}"
+
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/${REGIONS[$REGION]}/${ZONES[$ZONE]} /etc/localtime
+
+printf "Default locale is en_US.UTF-8 UTF-8. Would you like to change the locale or continue with default settings?\n\n>>> "
+read -r RESULT
+
+case $RESULT in
+        "Continue"|"continue"|"Yes"|"yes"|1)
+                arch-chroot /mnt sed -i 's/#\(en_US.UTF-8 UTF-8\)/\1/g' locale.gen
+                arch-chroot /mnt locale-gen
+        ;;
+        "Update"|"update"|"Change"|"change"|"No"|"no"|2)
+        ;;
+esac
+
+printf "Please enter a hostname for your device.\n\n>>> "
+read -r HOSTNAME
+
+echo $HOSTNAME >> /mnt/etc/hostname
+
+case $ENCRYPT in
+        "Yes"|"yes"|1)
+                printf "Update mkinitcpio.conf to support disk encryption and preload GFX drivers."
+                sed -i "s/MODULES=()/$MOD_NVIDIA/g" /mnt/etc/mkinitcpio.conf
+                sed -i "s/HOOKS=(.*)/$ENCRYPT_HOOKS/g" /mnt/etc/mkinitcpio.conf
+                arch-chroot /mnt mkinitcpio -P
+        ;;
+        "No"|"no"|2)
+        ;;
+esac
+while :
+do  
+        printf "Finally, please enter a password for your root account.\n\n>>> "
+        read -r -s PASSWD
+        clear
+        printf "Please confirm password.\n\n>>> "
+        read -r -s CONFPASSWD
+        if [[ $PASSWD == $CONFPASSWD ]]; then
+                arch-chroot /mnt passwd "$PASSWD"
+                break
+        else
+                printf "Passwords do not match. Please try again."
+                sleep 5
+                clear
+                continue
+        fi
+done
+
+printf "Please select a boot loader for your installation.\n\n"
+X=0
+for i in ${BOOT_LOADERS[@]}
+do
+        printf "\t%s\n" "$X) $i"
+        ((X++))
+done
+printf "\n\n>>> "
+read BOOT_LOADER
+
+case $BOOT_LOADER in
+        "GRUB"|"grub"|1)
+                grub-install --target-x86_64-efi --efi-directory /mnt/boot --bootloader-id=ArchLinux
+                grub-mkconfig -o /mnt/boot/grub/grub.cfg
+        ;;
+        "Systemd-Boot"|"systemd-boot"|2)
+        ;;
+esac
--- wrapper.sh
+++ wrapper.sh
@@ -17,27 +17,33 @@ do
 done
 
 #Get timezone information
-declare -A ZONES
+clear
 for z in /usr/share/zoneinfo/*
 do
-        REGION="$(grep -oP "[A-Z]{0,1}\w{0,20}$" <<< "$z")"
-        for i in "$z"/*
-        do
-                ZONE="$(grep -oP "[A-Z]{0,1}\w{0,20}$" <<< "$i")"
-                if [[ -z $ZONE ]]; then
-                        continue
-                fi
-                ZONES[$REGION]="${ZONES[$REGION]}${ZONES[$REGION]:+ }$ZONE"
-        done
+        REGION="$(grep -oP "[A-Z]{1}\w{0,20}$" <<< "$z")"
+        if [[ -z $REGION ]]; then
+                continue
+        fi
+        REGIONS+=("$(grep -oP "[A-Z]{1}\w{0,20}$" <<< "$z")")
+
         #ZONE="$(ls $z | grep -oP "[A-Z]{0,1}\w{0,20}$")"
-        REGIONS+="$z"
 done
 
 
-
-
+X=0
+for i in /usr/share/zoneinfo/"${REGIONS[$REGION]}"/*
+do
+        ZONE=$(grep -oP "[A-Z]{1}\w{0,20}$" <<< "$i")
+        if [[ -z $ZONE ]]; then
+                continue
+        fi
+        ZONES+=("$(grep -oP "[A-Z]{1}\w{0,20}$" <<< "$i")")
+done
 
 KERNELS=(linux-lts linux linux-hardened linux-rt linux-rt-lts linux-zen)
 BOOTMODE=$(cat /sys/firmware/efi/fw_platform_size)
 NTPSTATUS=$(timedatectl | grep -oP "(NTP service: )\K.*")
-export "${DRIVES?}" "${BOOTMODE?}" "${NTPSTATUS?}" "${KERNELS?}" "${ZONES?}" "${REGIONS?}"
+MOD_NVIDIA=(nvidia nvidia_modeset nvidia_drm nvidia_uvm)
+ENCRYPT_HOOKS=(base udev autodetect microcode modconf keyboard keymap consolefont block lvm2 encrypt filesystems fsck)
+BOOT_LOADERS=(GRUB Systemd-Boot)
+export "${DRIVES?}" "${BOOTMODE?}" "${NTPSTATUS?}" "${KERNELS?}" "${ZONES?}" "${REGIONS?}" "${MOD_NVIDIA?}" "${ENCRYPT_HOOKS?}" "${BOOT_LOADERS?}"
